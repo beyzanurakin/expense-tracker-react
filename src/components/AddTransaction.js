204c542d348bced4dd4677b2eae71c6b26e4e85a
beyzanurakin <akinbeyzanur@gmail.com>
2022-06-16 14:35:42 +0300

    Add components and redux files

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,51 @@
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
+const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
+
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000),
+      text,
+      //to convert number
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
